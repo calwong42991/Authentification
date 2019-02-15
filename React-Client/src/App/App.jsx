215a4fe8486e02cf6fb42dca80a4e3a60729fb20
Calvin Wong <calwong42991@gmail.com>
2019-02-14 18:45:06 -0800

    added App.jsx

---
+++ React-Client/src/App/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    
+    return (
+      <div>
+        <img alt="header" src="/dist/images/header.jpg" className="app-header" />
+        <h1> React Application</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
