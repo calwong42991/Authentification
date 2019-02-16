958510ddc0e3e0fbf5cccb63423adbca9768ffc7
Calvin Wong <calwong42991@gmail.com>
2019-02-15 18:21:00 -0800

    added store

--- React-Client/src/_helpers/store.js
+++ React-Client/src/_helpers/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import { ThunkMiddleware } from 'redux-thunk';
+import ThunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import {  } from '../_reducers';
+import rootReducer from '../_reducers';
 
 const loggerMiddleWare = createLogger();
 
-export const store = {
-  
-}
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    ThunkMiddleware,
+    loggerMiddleWare
+  )
+);
+
