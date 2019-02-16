172fba4460baf30bb62494d695a0a72da1e79f4e
Calvin Wong <calwong42991@gmail.com>
2019-02-15 20:23:10 -0800

    working on front end pages

--- React-Client/src/App/App.jsx
+++ React-Client/src/App/App.jsx
@@ -1,14 +1,54 @@
 import React, { Component } from 'react'
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const { dispatch } = this.props;
+    history.listen((location, action) => {
+      dispatch(alertActions.clear());
+    });
+  }
+
   render () {
-    
+    const { alert } = this.props;
     return (
-      <div>
-        <h1> React Application</h1>
-      </div>
-    )
+        <div className="jumbotron">
+            <div className="container">
+                <div className="col-sm-8 col-sm-offset-2">
+                    {alert.message &&
+                        <div className={`alert ${alert.type}`}>{alert.message}</div>
+                    }
+                    <Router history={history}>
+                        <div>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/register" component={RegisterPage} />
+                        </div>
+                    </Router>
+                </div>
+            </div>
+        </div>
+    );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+    alert
+  };
+}
+
+const connnectedApp = connect(mapStateToProps)(App);
+
+export { connnectedApp as App };
